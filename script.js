9272d85a9f2c08b4650c24c8da4a40992f9ccaa1
anish shetty <anishshetty901@gmail.com>
2024-08-31 18:58:52 +0530

    added files

---
+++ script.js
@@ -0,0 +1,72 @@
+const displayInput = document.querySelector(".display-input");
+const btnReset = document.querySelector(".btn-reset");
+const btnEqual = document.querySelector(".btn-equal");
+const btnDelete = document.querySelector(".btn-delete");
+const buttons = document.querySelectorAll(".button");
+
+const buttonsArray = Array.from(buttons);
+
+let lastKeyIsOperator = false;
+let decimalAdded = false;
+
+const keyClickHandler = (event) => {
+  const value = event.target.innerText;
+
+  if (value === "." && decimalAdded) {
+    return;
+  }
+
+  if ("+-x/".includes(value)) {
+    if (lastKeyIsOperator) {
+      let initialValue = displayInput.value;
+      let updatedValue = initialValue.substring(0, initialValue.length - 1) + value;
+      displayInput.value = updatedValue;
+      return;
+    }
+
+    lastKeyIsOperator = true;
+    decimalAdded = false;
+  } else {
+    lastKeyIsOperator = false;
+
+    if (value === ".") {
+      decimalAdded = true;
+    }
+  }
+
+  displayInput.value += value;
+  displayInput.scrollLeft = displayInput.scrollWidth;
+};
+
+const resetHandler = () => {
+  displayInput.value = "";
+  decimalAdded = false;
+};
+
+const deleteHandler = () => {
+  let initialValue = displayInput.value;
+  let updatedValue = initialValue.substring(0, initialValue.length - 1);
+  displayInput.value = updatedValue;
+};
+
+const expressionHandler = (expression) => {
+  const formattedExpression = expression.replace(/x/g, "*");
+  const result = eval(formattedExpression);
+
+  if (Number.isInteger(result)) {
+    return result.toString();
+  } else {
+    return parseFloat(result).toFixed(3);
+  }
+};
+
+const answerHandler = () => {
+  const expression = displayInput.value;
+  const result = expressionHandler(expression);
+  displayInput.value = result;
+};
+
+buttonsArray.forEach((button) => button.addEventListener("click", keyClickHandler));
+btnReset.addEventListener("click", resetHandler);
+btnDelete.addEventListener("click", deleteHandler);
+btnEqual.addEventListener("click", answerHandler);
